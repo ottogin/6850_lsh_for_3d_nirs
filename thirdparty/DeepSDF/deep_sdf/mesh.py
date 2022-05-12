f5a6644e3c727a5d0587efdf1e27db43e14c2765
Artem Lukoianov <arteml@symphony.csail.mit.edu>
2022-05-12 12:25:11 -0400

    NEW: final code version

--- thirdparty/DeepSDF/deep_sdf/mesh.py
+++ thirdparty/DeepSDF/deep_sdf/mesh.py
@@ -20,8 +20,9 @@ def create_mesh(
     decoder.eval()
 
     # NOTE: the voxel_origin is actually the (bottom, left, down) corner, not the middle
-    voxel_origin = [-1, -1, -1]
-    voxel_size = 2.0 / (N - 1)
+    or_s = 0.5
+    voxel_origin = [-or_s, -or_s, -or_s]
+    voxel_size = 1.0 / (N - 1)
 
     overall_index = torch.arange(0, N ** 3, 1, out=torch.LongTensor())
     samples = torch.zeros(N ** 3, 4)
@@ -55,11 +56,10 @@ def create_mesh(
         )
         head += max_batch
 
-    sdf_values = samples[:, 3]
+    sdf_values = samples[:, 3] - 0.005
     sdf_values = sdf_values.reshape(N, N, N)
 
     end = time.time()
-    print("sampling takes: %f" % (end - start))
 
     convert_sdf_samples_to_ply(
         sdf_values.data.cpu(),
@@ -93,16 +93,16 @@ def convert_sdf_samples_to_ply(
 
     numpy_3d_sdf_tensor = pytorch_3d_sdf_tensor.numpy()
 
-    verts, faces, normals, values = skimage.measure.marching_cubes_lewiner(
+    verts, faces, normals, values = skimage.measure.marching_cubes(
         numpy_3d_sdf_tensor, level=0.0, spacing=[voxel_size] * 3
     )
 
     # transform from voxel coordinates to camera coordinates
     # note x and y are flipped in the output of marching_cubes
     mesh_points = np.zeros_like(verts)
-    mesh_points[:, 0] = voxel_grid_origin[0] + verts[:, 0]
-    mesh_points[:, 1] = voxel_grid_origin[1] + verts[:, 1]
-    mesh_points[:, 2] = voxel_grid_origin[2] + verts[:, 2]
+    mesh_points[:, 1] = voxel_grid_origin[0] + verts[:, 0]
+    mesh_points[:, 2] = voxel_grid_origin[1] + verts[:, 1]
+    mesh_points[:, 0] = voxel_grid_origin[2] + verts[:, 2]
 
     # apply additional offset and scale
     if scale is not None:
